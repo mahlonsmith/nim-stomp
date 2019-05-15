ebd3e8fb9cab8fbf33862bb68c93a83a8dc84143
Mahlon E. Smith <mahlon@laika.com>
2019-05-15 14:54:55 -0700

    Version bump.

--- src/stomp.nim
+++ src/stomp.nim
@@ -1,6 +1,6 @@
 # vim: set et nosta sw=4 ts=4 ft=nim : 
 #
-# Copyright (c) 2016-2018, Mahlon E. Smith <mahlon@martini.nu>
+# Copyright (c) 2016-2019, Mahlon E. Smith <mahlon@martini.nu>
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -58,7 +58,7 @@ import
     uri
 
 const
-    VERSION = "0.1.1" ## The current program version.
+    VERSION = "0.1.2" ## The current program version.
     NULL    = "\x00"  ## The NULL character.
     CR      = "\r"    ## The carriage return character.
     CRLF    = "\r\n"  ## Carriage return + Line feed (EOL).
